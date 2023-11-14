3298adebaa75b393f080145bdcba7451c0f00be3
Kamiksss <117029713+Kamiksss@users.noreply.github.com>
2023-11-14 17:11:31 +0000

    saveFile ready and webScrap merged into index

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-
+import * as cheerio from 'cheerio';
 import { exit } from "process";
 
 
@@ -17,6 +17,44 @@ function readFile():string[]{
   }
 }
 
-const arrVocab = (wordlist: string ) => wordlist.split("\n");
+async function webScraper(arrVocab: string[]){
+  const promises =  arrVocab.map(async (word) => {
+    const response = await fetch(`https://www.thesaurus.com/browse/${word}`);
+    const html = await response.text();
+    const $ = await cheerio.load(html);
+    const sections = await $('section[data-type=thesaurus-synonyms-card]');
+    const list = await sections.children('ul').children('li');
+    const arr = await list.toArray().map((e) => $(e).text());
+    return arr;
+  });
+
+  const result = await Promise.all(promises);
+  let concat = [];
+  for(let i=0; i<arrVocab.length; i++){
+    concat[i]=[arrVocab[i],result[i]]
+  }
+
+  return concat
+ 
+}
+
+async function saveFile() {
+
+  const arrSynonyms = await webScraper(readFile())
+
+  fs.writeFileSync('./synonyms.txt', "")
+
+  for(let i=0; i<arrSynonyms.length; i++){
+    console.log(`${arrSynonyms[i][0]} appended`)
+    fs.appendFileSync('./synonyms.txt',`${arrSynonyms[i][0]}: ${arrSynonyms[i][1]} \n`)
+  }
+  
+}
+
+saveFile();
+
+
+
+
 
 
